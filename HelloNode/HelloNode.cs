daf69dfdc7a1f6f2a5561e84d1edb9fbec625928
Doug Klassen <dklassen>
2018-09-04 12:58:12 -0700

    Created first test zero-touch node and imported into a Dynamo graph

---
+++ HelloNode/HelloNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dougklassen.Dynamo.HelloNode
+{
+    public class HelloNode
+    {
+        public static String HelloMessage()
+        {
+            String msg = "Hello Dynamo!";
+            return msg;
+        }
+    }
+}
