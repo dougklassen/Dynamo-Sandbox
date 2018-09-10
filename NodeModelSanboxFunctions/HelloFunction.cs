f6049c0a44dc119a38ab7e6bad85f31bbeacd5dc
Doug Klassen <dklassen>
2018-09-10 08:07:24 -0700

    Added a NodeModel Hello World node

---
+++ NodeModelSanboxFunctions/HelloFunction.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.DesignScript.Runtime;
+
+namespace DougKlassen.Dynamo.NodeModelSandbox.Functions
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public class HelloFunction
+    {
+        public static string HelloMessage()
+        {
+            string msg = string.Empty;
+            msg += "Hello Dynamo!";
+            return (msg);
+        }
+    }
+}
---
+++ NodeModelSandbox/HelloNodeModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ProtoCore.AST.AssociativeAST;
+using Dynamo.Graph.Nodes;
+
+using DougKlassen.Dynamo.NodeModelSandbox.Functions;
+
+namespace DougKlassen.Dynamo.NodeModelSandbox
+{
+    [IsDesignScriptCompatible]
+    [NodeName("Hello Node")]
+    [NodeCategory("DougKlassen")]
+    [NodeDescription("Generates a hello world message")]
+    [NodeSearchTags(new String[] {
+        "Doug Klassen",
+        "Sandbox",
+        "Hello World"
+    })]
+    [OutPortNames("message")]
+    [OutPortTypes("string")]
+    [OutPortDescriptions("The generated message")]
+    public class HelloNodeModel : NodeModel
+    {
+        public HelloNodeModel()
+        {
+            RegisterAllPorts();
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            var functionCall =
+                AstFactory.BuildFunctionCall(
+                    new Func<string>(HelloFunction.HelloMessage),
+                    new List<AssociativeNode>());
+
+            return new AssociativeNode[] {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0),
+                functionCall)};
+        }
+    }
+}
